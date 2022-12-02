43fa05eb99a753ae08788580ff7580f20062c56f
Kel-VinTech <mondaykelvintech@gmail.com>
2022-12-02 18:56:31 -0500

    changes

---
+++ src/Data.js
@@ -0,0 +1,13 @@
+const Data = [
+    {
+        title:"Mount Fuji",
+        location:"Japan",
+        MapUrl:"https",
+        startDate:"12 Jan 2021",
+        endDate:"24 Jan 2021",
+        description:"this is mount fuji",
+        
+    }
+]
+
+export default Data
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Main = (props) => {
+    return(
+        <main className="row">
+            <div>
+                <h2>Japan</h2>
+                <a href="www.google.com"></a>
+            </div>
+            <h1>Mount Fuji</h1>
+            <h3>12 jan 2022 - 13 jan 2023</h3>
+            <p>hti isbdjb djvibvjvv vjvchvojcoiboev</p>
+
+        </main>
+    )
+}
+
+export default Main
