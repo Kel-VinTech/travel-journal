87b2b70d5f8cbf7ba6c150188f554f1e926bbf04
Kel-VinTech <mondaykelvintech@gmail.com>
2022-12-02 20:40:28 -0500

    changes

--- src/Data.js
+++ src/Data.js
@@ -1,34 +1,44 @@
-import image from "../src/image 12.png"
+import image1 from "../src/jake-nackos-fo3FPyUvejk-unsplash.jpg"
+import image2 from "../src/sophie-turner-IwZ_sfGe5wg-unsplash.jpg"
+import image3 from "../src/alexander-sinn-jgAaX0zkYCI-unsplash.jpg"
 
 const Data = [
     {
         title:"Mount Fuji",
-        location:"Japan",
+        location:"JAPAN",
         MapUrl:"https://goo.gl/maps/97QGsDVCJHxbihqHA",
         startDate:"12 Jan 2021",
         endDate:"24 Jan 2021",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!",
-        img:image
+        description:`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!`,         
+        img:image1
         
     },
     {
-        title:"Mount Fuji",
-        location:"Japan",
+        title:"Sydney Opera House",
+        location:"AUSTRALIA",
         MapUrl:"https://goo.gl/maps/97QGsDVCJHxbihqHA",
-        startDate:"12 Jan 2021",
-        endDate:"24 Jan 2021",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!",
-        img:image
+        startDate:"05 Jun 2021",
+        endDate:"24 Jun 2021",
+        description:`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!
+     Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!`, 
+        img:image2
         
     },
     {
-        title:"Mount Fuji",
-        location:"Japan",
+        title:"Geirangerfjord",
+        location:"NORWAY",
         MapUrl:"https://goo.gl/maps/97QGsDVCJHxbihqHA",
-        startDate:"12 Jan 2021",
-        endDate:"24 Jan 2021",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!",
-        img:image
+        startDate:"01 Oct 2021",
+        endDate:"18 Nov 2021",
+        description:`Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!
+        Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!
+     Excepturi animi dolorem cum autem facilis nobis aliquam numquam ullam quidem est!`,        img:image3
         
     }
 ]
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,14 +5,20 @@ const Main = (props) => {
     return(
         <main className="row">
             <img src={props.img} alt="woman"/>
-            <div className="local_stat">
-                <i class="fa-solid fa-location-dot"></i>
-                <h2>{props.location}</h2>
-                <a href="www.google.com">View on Google Maps</a>
+            <div className="main_content">
+                <div className="local_stat">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <h2>{props.location}</h2>
+                    <a href="www.google.com">View on Google Maps</a>
+                    
+                </div>
+                <div>
+                        <h1>{props.title}</h1>
+                        <h3>{props.startDate} - {props.endDate}</h3>
+                        <p>{props.description}</p>     
+                    </div>
             </div>
-            <h1>{props.title}</h1>
-            <h3>{props.startDate} - {props.endDate}</h3>
-            <p>{props.description}</p>
+            
 
         </main>
     )
